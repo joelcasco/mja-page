600c54d314f6189197e00b99b7ee0d3bf15d6e1a
joelcascoo <joel_casco@hotmail.com.ar>
2024-04-23 20:21:12 -0300

    Actualizacion de links en menu

--- script.js
+++ script.js
@@ -55,51 +55,51 @@ const toggleMenu = () => {
 // };
 
     
-let nextDom = document.getElementById('next');
-let prevDom = document.getElementById('prev');
-let carouselDom = document.querySelector('.carousel');
-let listItemDom = document.querySelector('.carousel .list');
-let thumbailDom = document.querySelector('.carousel .thumbail');
+// let nextDom = document.getElementById('next');
+// let prevDom = document.getElementById('prev');
+// let carouselDom = document.querySelector('.carousel');
+// let listItemDom = document.querySelector('.carousel .list');
+// let thumbailDom = document.querySelector('.carousel .thumbail');
 
-nextDom.onclick = function () {
-  showSlider('next');
-}
-prevDom.onclick = function(){
-  showSlider('prev')
-}
+// nextDom.onclick = function () {
+//   showSlider('next');
+// }
+// prevDom.onclick = function(){
+//   showSlider('prev')
+// }
 
-let timeRunnig = 3000;
-let timeAutoNext = 7000;
-let runTimeOut ; 
-let runAutoRun =setTimeout(()=>{
-  nextDom.click();
-}, timeAutoNext);
-function showSlider(type){
-  let itemSlider = document.querySelectorAll('.carousel .list .item');
-  let itemThumbail = document.querySelectorAll('.carousel .thumbail .item');
+// let timeRunnig = 3000;
+// let timeAutoNext = 7000;
+// let runTimeOut ; 
+// let runAutoRun =setTimeout(()=>{
+//   nextDom.click();
+// }, timeAutoNext);
+// function showSlider(type){
+//   let itemSlider = document.querySelectorAll('.carousel .list .item');
+//   let itemThumbail = document.querySelectorAll('.carousel .thumbail .item');
 
-  if (type == 'next') {
-    listItemDom.appendChild(itemSlider[0]);
-    thumbailDom.appendChild(itemThumbail[0]);
-    carouselDom.classList.add('next')
-  }else{
-    let positionLastItem = itemSlider.length -1;
-    listItemDom.prepend(itemSlider[positionLastItem])
-    thumbailDom.prepend(itemThumbail[positionLastItem])
-    carouselDom.classList.add('prev')
-  }
-  clearTimeout(runTimeOut);
-    runTimeOut =setTimeout(()=>{
-      carouselDom.classList.remove('next');
-      carouselDom.classList.remove('prev');
+//   if (type == 'next') {
+//     listItemDom.appendChild(itemSlider[0]);
+//     thumbailDom.appendChild(itemThumbail[0]);
+//     carouselDom.classList.add('next')
+//   }else{
+//     let positionLastItem = itemSlider.length -1;
+//     listItemDom.prepend(itemSlider[positionLastItem])
+//     thumbailDom.prepend(itemThumbail[positionLastItem])
+//     carouselDom.classList.add('prev')
+//   }
+//   clearTimeout(runTimeOut);
+//     runTimeOut =setTimeout(()=>{
+//       carouselDom.classList.remove('next');
+//       carouselDom.classList.remove('prev');
 
-    },timeRunnig)
-  clearTimeout(runAutoRun);
-  runAutoRun = setTimeout(()=>{
-    nextDom.click();
-  }, timeAutoNext)
+//     },timeRunnig)
+//   clearTimeout(runAutoRun);
+//   runAutoRun = setTimeout(()=>{
+//     nextDom.click();
+//   }, timeAutoNext)
 
-}
+// }
 
   const productContainers = [...document.querySelectorAll('.product-container')];
   const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
@@ -117,33 +117,7 @@ function showSlider(type){
           item.scrollLeft -= containerWidth;
       })
   })
-  let map;
-
-  function initMap() {
-    const mapOptions = {
-      center: { lat: -34.603722, lng: -58.381592 }, // Centro del mapa (latitud y longitud)
-      zoom: 12, // Nivel de zoom del mapa
-    };
-
-    // Crear el mapa y asignarlo al contenedor con el ID "map"
-    map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-    // Agregar marcadores para las localizaciones
-    const locations = [
-      { lat: -34.603722, lng: -58.381592, title: "Parada 1" },
-      { lat: -34.610856, lng: -58.405967, title: "Parada 2" },
-      { lat: -34.617633, lng: -58.362056, title: "Parada 3" },
-      // Agrega más localizaciones según sea necesario
-    ];
-
-    locations.forEach(location => {
-      new google.maps.Marker({
-        position: { lat: location.lat, lng: location.lng },
-        map,
-        title: location.title,
-      });
-    });
-  }
+  
 
 
 
